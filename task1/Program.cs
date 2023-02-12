5de99f64a39cb9ea4bd9948c870e40232df23d22
Aleksey <lexandreeev@gmail.com>
2023-02-12 16:33:24 +0300

    add task1

---
+++ task1/Program.cs
@@ -0,0 +1,30 @@
+﻿Console.WriteLine("Введите число n: ");
+
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите число m: ");
+
+int m = Convert.ToInt32(Console.ReadLine());
+
+double[,] array = new double[n, m];
+
+Random rand = new Random();
+
+for (int i = 0; i < n; i++)
+{
+  for (int j = 0; j < m; j++)
+  {
+    array[i, j] = rand.Next(1, 10) + rand.NextDouble();
+  }
+}
+
+for (int i = 0; i < n; i++)
+{
+  for (int j = 0; j < m; j++)
+  {
+    Console.Write(Math.Round(array[i,j], 2) + " | ");
+  }
+  Console.WriteLine();
+}
+
+Console.ReadKey();
